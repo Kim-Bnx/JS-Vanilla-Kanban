18b8c59019b8025abbd209d1556b0fa627061338
Kimberley Bonix <kimberley.bonix@oclock.school>
2024-01-02 21:43:45 +0100

    :rocket: deploying API

--- app/database.js
+++ app/database.js
@@ -1,11 +1,11 @@
 const { Sequelize } = require('sequelize');
 
 // Log sequelize to postgres DB
-const sequelize = new Sequelize(process.env.DB_URL, {
+const sequelize = new Sequelize(process.env.POSTGRES_URL, {
   dialect: "postgres",
   define: {
     underscored: true,
-  }
+  },
 });
 
 module.exports = sequelize;
